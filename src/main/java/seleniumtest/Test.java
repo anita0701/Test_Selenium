07d12b74de79d6f3ebed5d2dd8fb6c6a1e335b65
anita0701 <anita.dewangan@townscript.com>
2019-08-22 12:56:47 +0530

    First commit

---
+++ src/main/java/seleniumtest/Test.java
@@ -0,0 +1,18 @@
+package seleniumtest;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Test {
+
+	
+		 public static void main(String[] args) {
+			 
+			 System.setProperty("webdriver.chrome.driver","/home/anita/eclipse-workspace/seleniumtest/drivers/chromedriver");
+			 WebDriver driver = new ChromeDriver();
+			 driver.get("http://google.com");
+			 }		
+
+	
+
+}
