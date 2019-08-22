77e399259dca6c219d06825dfee6b458ebdaa50b
anita0701 <anita.dewangan@townscript.com>
2019-08-22 17:40:04 +0530

    new commit

--- src/main/java/seleniumtest/Test.java
+++ src/main/java/seleniumtest/Test.java
@@ -1,17 +1,18 @@
 package seleniumtest;
-
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class Test {
 
-	
-		 public static void main(String[] args) {
-			 
-			 System.setProperty("webdriver.chrome.driver","/home/anita/eclipse-workspace/seleniumtest/drivers/chromedriver");
+		 public static void main(String[] args) 
+		 {
+			  System.setProperty("webdriver.chrome.driver","/home/anita/eclipse-workspace/seleniumtest/drivers/chromedriver");
 			 WebDriver driver = new ChromeDriver();
 			 driver.get("http://google.com");
-			 }		
+			 String pageTitle = driver.getTitle();
+			 System.out.println("page title is :" + pageTitle);
+			 
+		 }		
 
 	
 
