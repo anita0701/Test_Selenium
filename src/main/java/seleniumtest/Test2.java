7d71589b13f2c386a134b93e59b4eedee5aa87fe
anita0701 <anita.dewangan@townscript.com>
2019-09-05 18:29:48 +0530

    testng.xml added

---
+++ src/main/java/seleniumtest/Test2.java
@@ -0,0 +1,12 @@
+package seleniumtest;
+
+public class Test2 {
+	
+	@Test
+	public void test2()
+	{
+		System.out.println("Login done");
+	}
+
+	
+}
--- src/main/java/seleniumtest/TestingSeleniumJenkins.java
+++ src/main/java/seleniumtest/TestingSeleniumJenkins.java
@@ -1,14 +1,16 @@
 package seleniumtest;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
 
-public class Test {
-
-		 public static void main(String[] args) 
+public class TestingSeleniumJenkins {
+        
+	     @Test
+		 public void test()
 		 {
 			  System.setProperty("webdriver.chrome.driver","/home/anita/eclipse-workspace/seleniumtest/drivers/chromedriver");
 			 WebDriver driver = new ChromeDriver();
-			 driver.get("http://google.com");
+			 driver.get("https://www.tsdugout.in/");
 			 String pageTitle = driver.getTitle();
 			 System.out.println("page title is :" + pageTitle);
 			 
