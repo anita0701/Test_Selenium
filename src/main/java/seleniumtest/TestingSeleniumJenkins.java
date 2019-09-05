78d8488c5e4abaa95b92ea29bb6fed03da74535c
anita0701 <anita.dewangan@townscript.com>
2019-09-05 19:07:50 +0530

    path set for driver

--- src/main/java/seleniumtest/TestingSeleniumJenkins.java
+++ src/main/java/seleniumtest/TestingSeleniumJenkins.java
@@ -4,11 +4,12 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
 
 public class TestingSeleniumJenkins {
-        
+	public static final String JENKINS_BUILD_PATH = "/var/lib/jenkins/workspace/MavenSeleniumDemoproject/";
 	     @Test
 		 public void test()
 		 {
-			  System.setProperty("webdriver.chrome.driver","/home/anita/eclipse-workspace/seleniumtest/drivers/chromedriver");
+			  
+	    	 System.setProperty("webdriver.chrome.driver",JENKINS_BUILD_PATH+"drivers/chromedriver");
 			 WebDriver driver = new ChromeDriver();
 			 driver.get("https://www.tsdugout.in/");
 			 String pageTitle = driver.getTitle();
