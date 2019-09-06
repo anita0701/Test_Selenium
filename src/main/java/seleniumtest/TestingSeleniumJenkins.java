30722912b5649c8b4e61dd98cd8c2bcccacc6e89
anita0701 <anita.dewangan@townscript.com>
2019-09-06 11:48:10 +0530

    chromedriver 76 added

--- src/main/java/seleniumtest/TestingSeleniumJenkins.java
+++ src/main/java/seleniumtest/TestingSeleniumJenkins.java
@@ -1,22 +1,26 @@
 package seleniumtest;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
 import org.testng.annotations.Test;
 
 public class TestingSeleniumJenkins {
 	public static final String JENKINS_BUILD_PATH = "/var/lib/jenkins/workspace/MavenSeleniumDemoproject/";
-	     @Test
-		 public void test()
-		 {
-			  
-	    	 System.setProperty("webdriver.chrome.driver",JENKINS_BUILD_PATH+"drivers/chromedriver");
-			 WebDriver driver = new ChromeDriver();
-			 driver.get("https://www.tsdugout.in/");
-			 String pageTitle = driver.getTitle();
-			 System.out.println("page title is :" + pageTitle);
-			 
-		 }		
+	@Test
+	public void test()
+	{
 
-	
+		DesiredCapabilities capability = DesiredCapabilities.chrome();
+		System.setProperty("webdriver.chrome.driver",JENKINS_BUILD_PATH+"drivers/chromedriver");
+		WebDriver driver = new ChromeDriver();
+		//WebDriver driver = new FirefoxDriver();
+		driver.get("https://www.tsdugout.in/");
+		String pageTitle = driver.getTitle();
+		System.out.println("page title is :" + pageTitle);
+
+	}		
+
+
 
 }
